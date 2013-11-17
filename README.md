9048249d89aa81981dd322b5c512bdf8545321ea
43081j <43081j@server.local>
2013-11-17 14:40:09 +0000

    removed jquery reliance

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
 pixelate.js
 ===
 
-**pixelate.js** is a simple jQuery plugin to pixelate any set of images and optionally reveal them on hover.
+**pixelate.js** is a simple library and jQuery plugin to pixelate any set of images and optionally reveal them on hover.
 
 [Demo here](http://43081j.github.io/pixelate/)
 
-Example
+Usage
 ===
+
+**pixelate.js** can be used with or without jQuery.
+
+```javascript
+// Following two lines are near identical
+$('img').pixelate();
+document.querySelector('img').pixelate();
+```
+
+Or via HTML data attributes:
 
 ```html
 <img src="test.jpg" width="200" height="200" data-pixelate>
-```
-
-You may also pixelate an image via JavaScript (with jQuery) like so:
-
-```javascript
-$('img#myimage').pixelate();
 ```
 
 Options
@@ -23,6 +27,7 @@ Options
 
 * `value` The percentage of pixelation to perform, a value between `0` and `1`
 * `reveal` Reveal the image on hover and remain revealed if clicked
+* `revealonclick` Reveal the image on click. When combined with `reveal`, it will remain revealed after being clicked.
 
 These options may be specified by data tags, like so:
 
--- dist/pixelate.js
+++ dist/pixelate.js
@@ -4,69 +4,100 @@
  * Pixelate images with ease
  * License: MIT
  */
-(function($) {
-	$.fn.extend({
-		pixelate: function() {
-			var defaults = {
-				value: 0.05,
-				reveal: true
-			};
-			var options = arguments[0] || {};
-			if(typeof options !== 'object') {
-				options = { value: parseInt(arguments[0]) };
+(function(window, $) {
+	var pixelate = function() {
+		var defaults = {
+			value: 0.05,
+			reveal: true,
+			revealonclick: false
+		};
+		var options = arguments[1] || {};
+		var element = this, //arguments[0],
+			elementParent = element.parentNode;
+		if(typeof options !== 'object') {
+			options = { value: parseInt(arguments[1]) };
+		}
+		options = (function() {
+			var opts = {};
+			for(var k in defaults) {
+				if(element.hasAttribute('data-' + k)) {
+					opts[k] = element.getAttribute('data-' + k);
+					continue;
+				}
+				if(k in options) {
+					opts[k] = options[k];
+					continue;
+				}
+				opts[k] = defaults[k];
 			}
-			options = $.extend(defaults, options);
-			return this.each(function() {
-				var img = this,
-					imgWidth = $(img).width(),
-					imgHeight = $(img).height(),
-					revealed = false;
-				var canv = document.createElement('canvas');
-				canv.width = imgWidth;
-				canv.height = imgHeight;
-				var ctx = canv.getContext('2d');
-				ctx.mozImageSmoothingEnabled = false;
-				ctx.webkitImageSmoothingEnabled = false;
-				ctx.imageSmoothingEnabled = false;
-				var opts = $.extend(options, (function() {
-					var o = {};
-					for(var i = 0; i < img.attributes.length; i++) {
-						o[img.attributes[i].name.replace(/^data\-/, '')] = img.attributes[i].value;
-					}
-					return o;
-				})());
-				var width = imgWidth * opts.value,
-					height = imgHeight * opts.value;
-				ctx.drawImage(img, 0, 0, width, height);
-				ctx.drawImage(canv, 0, 0, width, height, 0, 0, canv.width, canv.height);
-				$(img).hide();
-				$(img).before(canv);
-				if(opts.reveal && opts.reveal !== 'false') {
-					$(canv).click(function() {
-						revealed = !revealed;
-						if(revealed) {
-							ctx.drawImage(img, 0, 0, imgWidth, imgHeight);
-						} else {
-							ctx.drawImage(img, 0, 0, width, height);
-							ctx.drawImage(canv, 0, 0, width, height, 0, 0, canv.width, canv.height);
-						}
-					});
-					$(canv).hover(
-						function() {
-							if(revealed) return;
-							ctx.drawImage(img, 0, 0, imgWidth, imgHeight);
-						},
-						function() {
-							if(revealed) return;
-							ctx.drawImage(img, 0, 0, width, height);
-							ctx.drawImage(canv, 0, 0, width, height, 0, 0, canv.width, canv.height);
-						}
-					);
+			return opts;
+		})();
+		var display = element.style.display,
+			imgWidth = element.width,
+			imgHeight = element.height,
+			revealed = false;
+		var canv = document.createElement('canvas');
+		canv.width = imgWidth;
+		canv.height = imgHeight;
+		var ctx = canv.getContext('2d');
+		ctx.mozImageSmoothingEnabled = false;
+		ctx.webkitImageSmoothingEnabled = false;
+		ctx.imageSmoothingEnabled = false;
+		var width = imgWidth * options.value,
+			height = imgHeight * options.value;
+		ctx.drawImage(element, 0, 0, width, height);
+		ctx.drawImage(canv, 0, 0, width, height, 0, 0, canv.width, canv.height);
+		element.style.display = 'none';
+		elementParent.insertBefore(canv, element);
+		if(options.revealonclick !== false && options.revealonclick !== 'false') {
+			/*
+			 * Reveal on click
+			 */
+			canv.addEventListener('click', function(e) {
+				revealed = !revealed;
+				if(revealed) {
+					ctx.drawImage(element, 0, 0, imgWidth, imgHeight);
+				} else {
+					ctx.drawImage(element, 0, 0, width, height);
+					ctx.drawImage(canv, 0, 0, width, height, 0, 0, canv.width, canv.height);
 				}
 			});
 		}
-	});
-	$(window).on('load', function() {
-		$('img[data-pixelate]').pixelate();
-	});
-})(jQuery);
+		if(options.reveal !== false && options.reveal !== 'false') {
+			/*
+			 * Reveal on hover
+			 */
+			canv.addEventListener('mouseenter', function(e) {
+				if(revealed) return;
+				ctx.drawImage(element, 0, 0, imgWidth, imgHeight);
+			});
+			canv.addEventListener('mouseleave', function(e) {
+				if(revealed) return;
+				ctx.drawImage(element, 0, 0, width, height);
+				ctx.drawImage(canv, 0, 0, width, height, 0, 0, canv.width, canv.height);
+			});
+		}
+	};
+	window.HTMLImageElement.prototype.pixelate = pixelate;
+	if(typeof $ === 'function') {
+		$.fn.extend({
+			pixelate: function() {
+				return this.each(function() {
+					pixelate.apply(this, arguments);
+				});
+			}
+		});
+		$(window).on('load', function() {
+			$('img[data-pixelate]').pixelate();
+		});
+	} else {
+		document.addEventListener('DOMContentLoaded', function(e) {
+			var img = document.querySelectorAll('img[data-pixelate]');
+			for(var i = 0; i < img.length; i++) {
+				img[i].addEventListener('load', function() {
+					this.pixelate();
+				});
+			};
+		});
+	}
+})(window, typeof jQuery === 'undefined' ? null : jQuery);
